cdb47301efa3ec68c9f3a8bf00fb8445be9a91f1
AnisKamal <aniskamal52@gmail.com>
2023-10-18 12:07:28 +0100

    add id to user dto

--- src/main/java/com/colis/dto/UserDTO.java
+++ src/main/java/com/colis/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.colis.dto;
 
-public record UserDTO(String email,
+public record UserDTO(
+                    String id ,
+                    String email,
                      String name,
                       String urlPhoto) {
 
